a78e6e703535b0ba44ad575d1f24a1cf28653517
Jonas <jonas@fortalezatec.com.br>
2018-07-25 14:23:41 -0300

    Primerio Commit

---
+++ src/revisaopa_univates/RevisaoPA_UNIVATES.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisaopa_univates;
+
+/**
+ *
+ * @author jonasdhein
+ */
+public class RevisaoPA_UNIVATES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OLÁ MUNDO");    
+    }
+    
+}
