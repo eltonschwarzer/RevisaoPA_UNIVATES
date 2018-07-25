d01f87a2954fea7dfaec0f8ec769a68eca4e55c0
Jonas <jonas@fortalezatec.com.br>
2018-07-25 14:36:13 -0300

    Segundo commit

---
+++ src/revisaopa_univates/Conta.java
@@ -0,0 +1,51 @@
+package revisaopa;
+/**
+ *
+ * @author jonasdhein
+ */
+public class Conta {
+    
+    private String nome;
+    private int conta;
+    private Double saldo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome2) {
+        nome = nome2;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public void saque(Double valor){
+        saldo = saldo - valor;
+    }
+    
+    public void deposito(Double valor){
+        saldo = saldo + valor;
+    }
+    
+    public String toString(){
+        return "---\nConta: " + conta + "\n" +
+               "Nome: " + nome + "\n" + 
+               "Saldo: " + saldo +
+               "\n---";
+    }
+    
+}
---
+++ src/revisaopa_univates/Exercicio1.java
@@ -0,0 +1,44 @@
+package revisaopa;
+/**
+ *
+ * @author jonasdhein
+ */
+public class Exercicio1 {
+    
+    //metódo principal
+    public static void main(String[] args){
+        
+        System.out.println("Início do sistema");
+        
+        Conta conta1 = new Conta();
+        conta1.setConta(365);
+        conta1.setNome("Jonas Dhein");
+        conta1.setSaldo(1000.00);
+        
+        Conta conta2 = new Conta();
+        conta2.setConta(666);
+        conta2.setNome("Lucas Pandolfo");
+        conta2.setSaldo(100000.00);
+        
+        System.out.println(conta1.toString());
+        System.out.println(conta2.toString());
+        
+        System.out.println("Transf. $200 de Lucas para Jonas");
+        transferencia(200d, conta2, conta1);
+        
+        System.out.println(conta1.toString());
+        System.out.println(conta2.toString());
+        
+    }
+
+    private static void transferencia(double valor, Conta origem, Conta destino) {
+        try{
+            origem.saque(valor);
+            destino.deposito(valor);
+            
+        }catch(Exception e){
+            System.out.println("ERRO:" + e.getMessage());
+        }
+    }
+    
+}
